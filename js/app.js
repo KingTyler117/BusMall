be16ba60b12235d933b7c46141e819711e726041
Tyler King <tyman.king97@gmail.com>
2020-05-12 21:00:08 -0700

    Added a table

--- js/app.js
+++ js/app.js
@@ -4,68 +4,199 @@
 // Name of the product
 // File path of image
 
-function Product(productName,filePath) {
-  this.productName = productName;
-  this.filePath = filePath;
-  this.timesClicked = 0;
-  this.timesDisplayed = 0;
-  allProducts.push(this);
+var uniqueIndexArray = [];
+var allBusMall = [];
+var parentElement = document.getElementById('container');
+var totalVotes = 0;
+var names = [];
+var votes = [];
+var rounds = 5;
+
+// Render images to the dom
+
+function BusmallImage (name, extension) {
+  this.filepath = `images/${name}${extension}`;
+  this.alt = name;
+  this.votes = 0;
+  this.views = 0;
+  this.title = name;
+  allBusMall.push(this);
 
 }
 
+BusmallImage.prototype.render = function (){
 
-var totalClicks = 0;
+  var imageElement = document.createElement('img');
+  imageElement.src = this.filepath;
+  imageElement.alt = this.alt;
+  imageElement.title = this.title;
+  this.views++
+  parentElement.appendChild(imageElement);
+
+};
+
+
 
 //  Putting items all into an array.
 
-var allProducts = ('bag', '/images/bag.jpg');
-new Product ('banana', '/ images/banana.jpg');
-new Product ('boots' , '/images/bag.jpg'); 
-new Product ('bathroom' , '/images/bathroom.jpg');
-new Product ('breakfast' , '/images/breakfast.jpg');
-new Product ('bubblegum' , '/images/bubble gum.jpg');
-new Product ('chair' , '/images/chair.jpg');
-new Product ('cthulhu' , '/images/cthulhu.jpg');
-new Product ('dog-duck , '/images/dog-duck.jpg'); 
-new Product ('dragon' , '/images/dragon.jpg');
-new Product ('pen' , '/images/pen.jpg');
-new Product ('pet-sweep' , '/images/pet-sweep.jpg');
-new Product ('scissors' , '/images/scissors.jpg');
-new Product ('shark , '/images/shark.jpg');
-new Product ('sweep' , '/images/sweep.jpg');
-new Product ('tauntaun' , '/images/tauntaun.jpg');
-new Product ('unicorn' , '/images/unicorn.jpg');
-new Product ('usb' , '/images/usb.jpg');
-new Product ('unicorn' , '/images/unicorn.jpg');
-new Product ('water-can' , '/images/water-can.jpg');
-new Product ('wine-glass' , '/images/wine-glass.jpg');
+new BusmallImage('bag', '.jpg');
+new BusmallImage ('banana', '.jpg');
+new BusmallImage ('boots' , '.jpg');
+new BusmallImage ('bathroom' , '.jpg');
+new BusmallImage('breakfast' , '.jpg');
+new BusmallImage ('bubblegum' , '.jpg');
+new BusmallImage ('chair' , '.jpg');
+new BusmallImage ('cthulhu' , '.jpg');
+new BusmallImage ('dog-duck' , '.jpg');
+new BusmallImage ('dragon' , '.jpg');
+new BusmallImage ('pen' , '.jpg');
+new BusmallImage ('pet-sweep' , '.jpg');
+new BusmallImage ('scissors' , '.jpg');
+new BusmallImage ('shark' , '.jpg');
+new BusmallImage ('sweep' , '.png');
+new BusmallImage ('tauntaun' , '.jpg');
+new BusmallImage('unicorn' , '.jpg');
+new BusmallImage ('usb' , '.gif');
+new BusmallImage ('unicorn' , '.jpg');
+new BusmallImage ('water-can' , '.jpg');
+new BusmallImage ('wine-glass' , '.jpg');
 
-ProductImage.prototype.products = function()[
-    // creating a element img
-    var imageElement = document.createElement('img');
-    imageElement.setAttribute('src', this.filePath');
-    imageElement.setAttribute('alt' , this.alt);
-    parent.appendChild(imageElement);
-]
+function getRandomIndex (){
 
-// helper function 
-function randomNumber (min=0,max){
-    return Math.floor(Math.random() * (max-min + 1)) + min;
+  var index = getRandomNumber (allBusMall.length);
+  // While loop will will check to make sure that my index is unique will get a index if it is in the uniqueIndexArray
+  while(uniqueIndexArray.includes (index)){
+    index = getRandomNumber (allBusMall.length);
+  }
+
+  uniqueIndexArray.push(index);
+
+  // if my array is more than 6 items long, I need to shift from the beginning
+
+  if(uniqueIndexArray.length > 3 ){
+    uniqueIndexArray.shift();
+  }
+
+  // / select a random image from that array to render
+  // helper function to generate a random number between 0 and the length of the array
+  // render the object instance at that index to the DOM
+
+  // make sure that the images are unique (to each other and previous images)
+  // -- come back to this in a minute
+
+  return index;
 }
 
-function getRandomProduct(){
-    parent.textContent = '';
+function getRandomNumber (max) {
+
+  return Math.floor(Math.random() * max);
 }
 
-// Call my random number function to get a random index position. That will be my random index position.
-
-var randomIndex === (0,allProducts.length-1);
-
-var secondRandomIndex = randomNumber (0, allProducts.length-1 );
-
-allProducts[secondRandomIndex] .products();
-allProducts[secondRandomIndex] .views++;
+function displayImage(){
+  var index = getRandomIndex();
+  allBusMall[index].render();
 }
 
-getRandomProduct();
+function handleClick(event){
+  // first - empty everything out
+  parentElement.textContent = '';
 
+  // figure out what was clicked
+  var titleOfTheThingThatWasClickedOn = event.target.title;
+
+  //  Loop through all of my object instance to compare titles so that I can find the one that was clicked on
+
+  for(var i=0; i<allBusMall.length; i++) {
+    if (titleOfTheThingThatWasClickedOn === allBusMall[i].title){
+      // To add a vote
+
+      allBusMall[i].votes++;
+      totalVotes++;
+    }
+
+  }
+  if(totalVotes === rounds) {
+    // To turn off the event listener
+
+    parentElement.removeEventListener ('click' ,handleClick);
+    makeNamesArray();
+
+  } else {
+    displayImage();
+    displayImage();
+    displayImage();
+  }
+
+  // listen for a click and render new images when we hear it
+  // need to keep track of views and votes
+
+}
+
+displayImage();
+displayImage();
+displayImage();
+
+parentElement.addEventListener ('click' , handleClick);
+
+
+// only allow 25 votes
+// show results at the end
+
+// / loop over all of my items and make an array of just the names of my items
+
+var names = new Array();
+console.log(names.length);
+
+var votes = new Array ();
+console.log(votes.length);
+
+
+
+function makeNamesArray(){
+  for(var i=0; i<allBusMall.length; i++){
+    names.push(allBusMall[i].title);
+    votes.push(allBusMall[i].votes);
+  }
+
+  generateChart ();
+}
+
+function generateChart(){
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: names,
+      datasets: [{
+        label: '# of Votes',
+        data: votes,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
+}
